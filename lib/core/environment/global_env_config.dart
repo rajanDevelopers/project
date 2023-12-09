8dc7f4f9b1def96ffe37d7cfb913a973863927c2
rajanDevelopers <rajan9523459381@gmail.com>
2023-12-09 18:40:16 +0000

    Code Synced by DhiWise

---
+++ lib/core/environment/global_env_config.dart
@@ -0,0 +1,6 @@
+import 'base_env_config.dart';
+
+class GlobalEnvConfig extends BaseEnvConfig {
+  @override
+  String get name => '5';
+}
---
+++ lib/presentation/adacana_screen/bloc/adacana_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import '/core/app_export.dart';
+import 'package:tudoapp/presentation/adacana_screen/models/adacana_model.dart';
+part 'adacana_event.dart';
+part 'adacana_state.dart';
+
+/// A bloc that manages the state of a Adacana according to the event that is dispatched to it.
+class AdacanaBloc extends Bloc<AdacanaEvent, AdacanaState> {
+  AdacanaBloc(AdacanaState initialState) : super(initialState) {
+    on<AdacanaInitialEvent>(_onInitialize);
+  }
+
+  _onInitialize(
+    AdacanaInitialEvent event,
+    Emitter<AdacanaState> emit,
+  ) async {}
+}
